aa8251f93e6a76543dc4deac283c79e75d4cdc90
Eray Hanoglu <e.hanoglu@panates.com>
2017-05-23 18:40:55 +0300

    - Fix raw issue in update statement

--- test/serialize-update.js
+++ test/serialize-update.js
@@ -29,4 +29,46 @@ describe('Serialize update statements', function () {
         done();
     });
 
+    it('should use Raw in table', function (done) {
+        let statement = sqb.update(sqb.raw('table1')).set({
+            name: 'name',
+            address: 'earth'
+        }).where(and('id', 1));
+        let result = statement.build();
+        assert.equal(result.sql, "update table1 set NAME = 'name', ADDRESS = 'earth' where id = 1");
+        done();
+    });
+
+    it('should use Raw in values', function (done) {
+        let statement = sqb.update('table1').set(sqb.raw("NAME = 'name', ADDRESS = 'earth'")).where(and('id', 1));
+        let result = statement.build();
+        assert.equal(result.sql, "update table1 set NAME = 'name', ADDRESS = 'earth' where id = 1");
+        done();
+    });
+
+    it('should check invalid argument for in "set"', function (done) {
+        let ok;
+        try {
+            sqb.update('table1').set(1).where();
+        } catch (e) {
+            ok = true;
+        }
+
+        assert.ok(ok);
+        done();
+    });
+
+    it('should pretty print', function (done) {
+        let statement = sqb.update('table1').set({
+            name: 'name',
+            address: 'earth'
+        }).where(and('id', 1));
+        let result = statement.build({
+            prettyPrint: true
+        });
+        assert.equal(result.sql, "update table1 set\n    NAME = 'name', ADDRESS = 'earth'\nwhere id = 1");
+        done();
+    });
+
+
 });
