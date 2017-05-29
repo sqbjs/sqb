8e22040b52a850108a60d4ac091234f1bf575b3d
Eray Hanoglu <e.hanoglu@panates.com>
2017-05-29 18:01:26 +0300

    Update tests

--- test/serializer.js
+++ test/serializer.js
@@ -6,11 +6,9 @@ describe('Serializer', function () {
 
     it('should configure', function (done) {
         let serializer = sqb.serializer({
-            dialect: 'oracle',
             prettyPrint: 1,
             namedParams: 1
         });
-        assert.equal(serializer.dialect, 'oracle');
         assert.equal(serializer.prettyPrint, true);
         assert.equal(serializer.namedParams, true);
         done();
@@ -72,28 +70,12 @@ describe('Serializer', function () {
         done();
     });
 
-    describe('Dialects', function () {
 
-        it('should initialize generic serializer', function (done) {
-            let obj = sqb.serializer();
-            assert.ok(obj instanceof sqb.Serializer);
-            done();
-        });
-
-        it('should initialize "oracle" dialect', function (done) {
-            let obj = sqb.serializer('oracle');
-            assert.ok(obj instanceof sqb.Serializer);
-            assert.equal(obj.dialect, 'oracle');
-            done();
-        });
-
-        it('should return serializer that already passed in first argument', function (done) {
-            let obj = sqb.serializer('oracle');
-            let obj2 = sqb.serializer(obj);
-            assert.ok(obj === obj2);
-            done();
-        });
-
+    it('should return serializer that already passed in first argument', function (done) {
+        let obj = sqb.serializer();
+        let obj2 = sqb.serializer(obj);
+        assert.ok(obj === obj2);
+        done();
     });
-
+    
 });
