b90c189d8c7dd8ab29b696f92bc876bb6eae58fd
Eray Hanoğlu <erayszn@gmx.net>
2020-12-01 16:21:14 +0300

    Capturing npm errors

--- support/gulp/tasks/publish.js
+++ support/gulp/tasks/publish.js
@@ -5,11 +5,32 @@ const {packages} = require('../package');
 module.exports = {
   ...packages.createTasks('publish', async (pkg) => {
         console.log(`publish '${colors.cyan(pkg.name)}'`);
-        await pkg.execSh('npm publish --access public', {
-          _onMessage: (msg)=> {
-            console.log('MESSAGE', msg);
+        let ignoreError = false;
+        let errMsg = '';
+        try {
+          await pkg.execSh('npm publish --access public', {
+            stdio: 'pipe',
+            onSpawn: (cp) => {
+              cp.stderr.setEncoding('utf8');
+              cp.stderr.on('data', (data) => {
+                if (data.includes('npm ERR!')) {
+                  errMsg += data.replace(/npm ERR!/g, '');
+                }
+                if (data.includes('403 Forbidden') &&
+                    data.includes('previously published')) {
+                  console.log(colors.yellow('Package already published!'));
+                  ignoreError = true;
+                }
+              });
+            }
+          });
+        } catch (e) {
+          //e.stack = '';
+          if (!ignoreError) {
+            process.stderr.write(errMsg);
+            throw e;
           }
-        });
+        }
       }
   )
 };
