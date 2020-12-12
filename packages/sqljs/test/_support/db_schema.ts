a264e6ae8edc4d71c761e813330358c9b25e5fc3
Eray Hanoğlu <erayszn@gmx.net>
2020-12-13 00:27:19 +0300

    Implemented find(), findOne() and findByPk()

--- packages/sqljs/test/_support/db_schema.ts
+++ packages/sqljs/test/_support/db_schema.ts
@@ -29,4 +29,28 @@ CREATE TABLE region_airports
     airport_id text,
     PRIMARY KEY (region_id, airport_id)
 );
+
+CREATE TABLE countries
+(
+    name text,
+    region text,
+    PRIMARY KEY (name)
+);
+
+insert into countries (name, region) values ('United States', 'US');
+insert into countries (name, region) values ('Turkey', 'TR');
+insert into countries (name, region) values ('Canada', 'CA');
+insert into countries (name, region) values ('Germany', 'DE');
+
+CREATE TABLE customers
+(
+    id INTEGER PRIMARY KEY,
+    given_name text,
+    family_name text,
+    birth_date date,
+    city text,
+    country text,
+    FOREIGN KEY (country) REFERENCES countries (name)
+);
+
 `
