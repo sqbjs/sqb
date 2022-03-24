f1f2af27088b4aa70c249b0db7235814b0c82a7a
Eray Hanoğlu <e.hanoglu@panates.com>
2022-03-24 22:59:26 +0300

    Updated dependencies

--- gulpfile.mjs
+++
@@ -1,6 +0,0 @@
-/**
- * Load the TypeScript compiler, then load the TypeScript gulpfile which simply loads all
- * the tasks. The tasks are really inside tools/gulp/tasks.
- */
-
-export * from './support/gulp/gulpfile.mjs';
