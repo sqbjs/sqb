943c0f2a49a8119c6f6fa93e9704cca0e55ff646
Eray Hanoğlu <e.hanoglu@panates.com>
2022-04-14 01:10:26 +0300

    Use Reflect to store Entity metadata (final)

--- packages/connect/src/index.ts
+++ packages/connect/src/index.ts
@@ -1,4 +1,5 @@
 import 'reflect-metadata';
+
 export {Type, Maybe, DeepPartial, DeepPickWritable, DeepBuildable} from 'ts-gems';
 export * from './types';
 
@@ -7,29 +8,39 @@ export * from './client/Adapter';
 export * from './client/SqbClient';
 export * from './client/SqbConnection';
 export * from './client/Cursor';
-export {registerAdapter, unRegisterAdapter} from './client/extensions';
+export {
+    registerAdapter,
+    unRegisterAdapter
+} from './client/extensions';
 
 export * from './orm/orm.type';
+export * from './orm/orm.const';
+export * from './orm/base-entity';
+export * from './orm/repository.class';
 
+export * from './orm/model/entity-metadata';
 export * from './orm/model/element-metadata';
 export * from './orm/model/column-element-metadata';
 export * from './orm/model/embedded-element-metadata';
 export * from './orm/model/association-element-metadata';
+export * from './orm/model/association';
+export * from './orm/model/association-node';
+export * from './orm/model/index-metadata';
+export * from './orm/model/link-chain';
 
-export * from './orm/base-entity.class';
-export * from './orm/decorators/entity.decorator';
-export * from './orm/decorators/primarykey.decorator';
-export * from './orm/decorators/index.decorator';
 export * from './orm/decorators/column.decorator';
-export * from './orm/decorators/link.decorator';
 export * from './orm/decorators/embedded.decorator';
-export * from './orm/decorators/foreignkey.decorator';
+export * from './orm/decorators/entity.decorator';
 export * from './orm/decorators/events.decorator';
+export * from './orm/decorators/foreignkey.decorator';
+export * from './orm/decorators/index.decorator';
+export * from './orm/decorators/link.decorator';
+export * from './orm/decorators/primarykey.decorator';
 export * from './orm/decorators/transform.decorator';
 
-export * from './orm/repository.class';
-export * from './orm/model/entity-metadata';
-export * from './orm/model/association';
-export * from './orm/model/index-metadata';
-
-export {isColumnElement, isEmbeddedElement, isAssociationElement, isEntityClass} from './orm/util/orm.helper';
+export {
+    isColumnElement,
+    isEmbeddedElement,
+    isAssociationElement,
+    isEntityClass
+} from './orm/util/orm.helper';
