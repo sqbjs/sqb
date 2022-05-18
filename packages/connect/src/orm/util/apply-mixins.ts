a44206e0f6d8148fab9187de96e618e70894bb25
Eray Hanoğlu <e.hanoglu@panates.com>
2022-05-18 15:08:12 +0300

    Fixed mixin issue

--- packages/connect/src/orm/util/apply-mixins.ts
+++ packages/connect/src/orm/util/apply-mixins.ts
@@ -1,7 +1,8 @@
 export function applyMixins(derivedCtor: any, baseCtor: any, filter?: (k: string) => boolean) {
     for (const name of Object.getOwnPropertyNames(baseCtor.prototype)) {
-        if (filter && !filter(name))
-            continue;
+        if ((name === 'constructor' || name === '__proto__' || name === 'toJSON' || name === 'toString') ||
+            filter && !filter(name)
+        ) continue;
         Object.defineProperty(
             derivedCtor.prototype,
             name,
