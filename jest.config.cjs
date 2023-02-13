32112894397309a211b9836f4d3961903cd4706c
Eray Hanoğlu <e.hanoglu@panates.com>
2023-02-13 14:52:11 +0300

    Updated dependencies

--- jest.config.cjs
+++ jest.config.cjs
@@ -2,8 +2,19 @@ const base = require('./jest.config.base.cjs');
 
 module.exports = {
   ...base,
+  verbose: true,
   projects: [
     '<rootDir>/packages/*/jest.config.cjs'
   ],
-  globalSetup: '<rootDir>/support/jest-setup.js'
+  globalSetup: '<rootDir>/support/jest-setup.js',
+  coveragePathIgnorePatterns: [
+    '/build/',
+    '/dist/',
+    '/packages/oracle/',
+    '/node_modules/',
+    '_support',
+    '_shared'
+  ],
+  coverageReporters: ['lcov', 'text'],
+  coverageDirectory: "<rootDir>/coverage/"
 };
