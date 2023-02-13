88cb95f044e434c0c49821d3c098fe1942c9a986
Eray Hanoğlu <e.hanoglu@panates.com>
2023-02-13 14:52:11 +0300

    Updated dependencies

--- jest.config.base.cjs
+++ jest.config.base.cjs
@@ -1,17 +1,6 @@
 module.exports = {
   testEnvironment: 'node',
-  verbose: true,
   maxWorkers: '50%',
-  coveragePathIgnorePatterns: [
-    '/build/',
-    '/dist/',
-    '/packages/oracle/',
-    '/node_modules/',
-    '_support',
-    '_shared'
-  ],
-  coverageReporters: ['lcov', 'text'],
-  coverageDirectory: "<rootDir>/coverage/",
   transform: {
     '^.+.ts?$': ['ts-jest', {
       tsconfig: '<rootDir>/test/tsconfig.json',
