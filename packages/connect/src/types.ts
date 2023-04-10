67abba9072ae7ad93df1946881f278570cebe6f7
Eray Hanoğlu <e.hanoglu@panates.com>
2023-04-10 14:37:12 +0300

    Fix: EntityOutput should not expose null fields.

--- packages/connect/src/types.ts
+++ packages/connect/src/types.ts
@@ -1,11 +1,18 @@
 import {Builtin, DeepPickWritable} from 'ts-gems';
 
-export type EntityInput<T> = DeepNullableIfPartial<DeepPickWritable<T>>;
-export type EntityOutput<T> = DeepNullableIfPartial<T>;
+export type EntityInput<T> = DeepNullablePartial<DeepPickWritable<T>>;
+export type EntityOutput<T> = DeepPartial<T>;
 
-export type DeepNullableIfPartial<T> = _DeepNullableIfPartial<T>;
-type _DeepNullableIfPartial<T> =
+export type DeepNullablePartial<T> = _DeepNullablePartial<T>;
+type _DeepNullablePartial<T> =
     T extends Builtin ? T
-        : T extends Promise<infer U> ? Promise<DeepNullableIfPartial<U>>
-            : T extends (infer U)[] ? DeepNullableIfPartial<U>[]
-                : { [P in keyof T]?: DeepNullableIfPartial<Exclude<T[P], undefined>> | null };
+        : T extends Promise<infer U> ? Promise<DeepNullablePartial<U>>
+            : T extends (infer U)[] ? DeepNullablePartial<U>[]
+                : { [P in keyof T]?: DeepNullablePartial<Exclude<T[P], undefined>> | null };
+
+export type DeepPartial<T> = _DeepPartial<T>;
+type _DeepPartial<T> =
+    T extends Builtin ? T
+        : T extends Promise<infer U> ? Promise<DeepPartial<U>>
+            : T extends (infer U)[] ? DeepPartial<U>[]
+                : { [P in keyof T]?: DeepPartial<Exclude<T[P], undefined>>};
