107311f0fe1e099255331e36d4b279b8d0cab10e
Eray Hanoğlu <e.hanoglu@panates.com>
2023-04-24 12:35:04 +0300

    Improved guessing association targetKey and sourceKey

--- packages/connect/test/orm/model/association.spec.ts
+++ packages/connect/test/orm/model/association.spec.ts
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/unbound-method,@typescript-eslint/no-unused-vars */
-import {Association, Column, ForeignKey, PrimaryKey} from '@sqb/connect';
+import {Association, Column, Entity, ForeignKey, PrimaryKey} from '@sqb/connect';
 
 class Country {
     @PrimaryKey()
@@ -10,13 +10,26 @@ class Country {
     name: string;
 }
 
-class Customer {
+class Record {
     @PrimaryKey()
     @Column()
     id: number;
+}
+
+class Deletable {
+    @Column()
+    deleted: boolean;
+}
+
+class Customer extends Entity.Union(Record, Deletable) {
 
     @Column()
     countryCode: string;
+}
+
+class CustomerNotes extends Record {
+    @Column()
+    customerId: number;
 }
 
 describe('Model / Association', function () {
@@ -57,25 +70,13 @@ describe('Model / Association', function () {
         expect(target.ctor).toStrictEqual(Country);
     });
 
-    it(`should determine sourceKey from target's primary index (camel-case)`, async () => {
-        const association = new Association('', {source: Customer, target: Country});
+    it(`should guess sourceKey and targetKey (camel-case)`, async () => {
+        let association = new Association('', {source: Customer, target: Country});
         expect(await association.resolveSourceKey()).toStrictEqual('countryCode');
-    });
-
-    it(`should determine sourceKey from target's primary index (snake-case)`, async () => {
-        class Customer2 {
-            @Column()
-                // eslint-disable-next-line camelcase
-            country_code: string;
-        }
-
-        const association = new Association('', {source: Customer2, target: Country});
-        expect(await association.resolveSourceKey()).toStrictEqual('country_code');
-    });
-
-    it(`should determine targetKey using target's primary index`, async () => {
-        const association = new Association('', {source: Customer, target: Country});
         expect(await association.resolveTargetKey()).toStrictEqual('code');
+        association = new Association('', {source: Customer, target: CustomerNotes, many: true});
+        expect(await association.resolveSourceKey()).toStrictEqual('id');
+        expect(await association.resolveTargetKey()).toStrictEqual('customerId');
     });
 
     it(`should determine sourceKey and targetKey using target's foreign keys`, async () => {
@@ -94,6 +95,19 @@ describe('Model / Association', function () {
         const association = new Association('', {source: Customer2, target: CustomerPhone});
         expect(await association.resolveSourceKey()).toStrictEqual('id');
         expect(await association.resolveTargetKey()).toStrictEqual('idOfCustomer');
+    });
+
+
+    it(`should guess sourceKey targetKey (snake-case)`, async () => {
+        class Customer2 {
+            @Column()
+                // eslint-disable-next-line camelcase
+            country_code: string;
+        }
+
+        const association = new Association('', {source: Customer2, target: Country});
+        expect(await association.resolveSourceKey()).toStrictEqual('country_code');
+        expect(await association.resolveTargetKey()).toStrictEqual('code');
     });
 
     it(`should determine keyColumn if there is no foreign-key`, async () => {
@@ -117,4 +131,5 @@ describe('Model / Association', function () {
         const association = new Association('', {source: Customer2, target: Country});
         expect(await association.resolveTargetKey()).toStrictEqual('code');
     });
+
 });
