2e14df420ec573cd8b8d59aacb0a4d1cb236025d
Eray Hanoğlu <e.hanoglu@panates.com>
2023-12-04 13:51:30 +0300

    Updated dependencies

--- packages/connect/src/types.ts
+++ packages/connect/src/types.ts
@@ -1,18 +1,7 @@
-import {Builtin, DeepPickWritable} from 'ts-gems';
+import { DeepPickWritable, HighDeepNullish } from 'ts-gems';
 
-export type EntityInput<T> = DeepNullablePartial<DeepPickWritable<T>>;
-export type EntityOutput<T> = DeepPartial<T>;
+export type PartialInput<T> = HighDeepNullish<DeepPickWritable<T>>;
+export type PartialOutput<T> = HighDeepNullish<T>;
 
-export type DeepNullablePartial<T> = _DeepNullablePartial<T>;
-type _DeepNullablePartial<T> =
-    T extends Builtin ? T
-        : T extends Promise<infer U> ? Promise<DeepNullablePartial<U>>
-            : T extends (infer U)[] ? DeepNullablePartial<U>[]
-                : { [P in keyof T]?: DeepNullablePartial<Exclude<T[P], undefined>> | null };
-
-export type DeepPartial<T> = _DeepPartial<T>;
-type _DeepPartial<T> =
-    T extends Builtin ? T
-        : T extends Promise<infer U> ? Promise<DeepPartial<U>>
-            : T extends (infer U)[] ? DeepPartial<U>[]
-                : { [P in keyof T]?: DeepPartial<Exclude<T[P], undefined>>};
+export type EntityInput<T> = PartialInput<T>;
+export type EntityOutput<T> = PartialOutput<T>;
